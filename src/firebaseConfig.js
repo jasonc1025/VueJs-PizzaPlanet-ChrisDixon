a75e701ae4b816276b81406fdd738fb84caff98e
jasonc1025 <jasonc@eande.world>
2018-03-19 11:01:43 -0700

    Add Firebase Customization

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,7 +1,13 @@
 import Firebase from 'firebase'
 
 var config = {
-
+    // [jwc]+x Add Firebase Customization
+    apiKey: "AIzaSyC2YkSfQY0F7YKxXCU1NR17IOQBuk6UDVs",
+    authDomain: "pizzaplanet-vuejs-chrisd.firebaseapp.com",
+    databaseURL: "https://pizzaplanet-vuejs-chrisd.firebaseio.com",
+    projectId: "pizzaplanet-vuejs-chrisd",
+    storageBucket: "pizzaplanet-vuejs-chrisd.appspot.com",
+    messagingSenderId: "807451204942"
 };
 const firebaseApp = Firebase.initializeApp(config);
 const db = firebaseApp.database()
